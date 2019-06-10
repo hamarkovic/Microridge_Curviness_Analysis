c5003a843661663b540577f2aebff46edb9e9daf
hamarkovic <49170854+hamarkovic@users.noreply.github.com>
2019-06-09 20:08:48 -0700

    Update concatenate.sh

--- Scripts/concatenate.sh
+++ Scripts/concatenate.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # This bash script adds the name of csv files to the beginning of each row in the csv file, creating a column with a file "ID"
 # It then concatenates the files.
-# Store all files to process as a csv file in the directory "Files_to_analyze"
+# It only keeps the header of the first file, and names the new column "Cell".
+# Store all files to process as csv files with headers in the directory "Files_to_analyze"
 # To run, type: $ sh concatenate.sh
 
 echo "... File concatenation begun"
@@ -11,17 +12,18 @@ cp -r Files_to_analyze Processing
 first=1
 for cell in Processing/Files_to_analyze/*
 do
-	fileID=$cell
-	fileID=$(echo $fileID | sed 's/Processing\/Files_to_analyze\///' | sed 's/.csv//')
-	if [ $first -eq 1 ]
-	then
-		cat $cell | sed "s/^/$fileID,/" > Concatenated_to_analyze.csv
-		first=2
-		echo $fileID.csv processed first
-	else
-		cat $cell | sed "s/^/$fileID,/" >> Concatenated_to_analyze.csv
+        fileID=$cell
+        fileID=$(echo $fileID | sed 's/Processing\/Files_to_analyze\///' | sed 's/.csv//')
+        if [ $first -eq 1 ]
+        then
+                head -n 1 $cell | sed "s/^/Cell,/" > Concatenated_to_analyze.csv
+                tail -n +2 $cell | sed "s/^/$fileID,/" >> Concatenated_to_analyze.csv
+                first=2
+                echo $fileID.csv processed first
+        else
+                tail -n +2 $cell | sed "s/^/$fileID,/" >> Concatenated_to_analyze.csv
                 echo $fileID.csv processed
-	fi
+        fi
 done
 
 echo "... All files processed"
